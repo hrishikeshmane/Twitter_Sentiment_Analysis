8d3f23785e2673ff0d64b83ab83dce4029d18ab9
Hrishi <hrishikeshmane87933@gmail.com>
2019-04-28 20:42:02 +0530

    changes

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def tweets():
         return jsonify({'data': tweets, 'count': len(tweets)})
 
 
-#port = int(os.environ.get('PORT', 5000))
-#app.run(host="0.0.0.0", port=port, debug=True)
-if __name__ == '__main__':
-    app.run()
+port = int(os.environ.get('PORT', 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
+#if __name__ == '__main__':
+#    app.run()
